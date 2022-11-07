1d17914d457e22b792a9ecc019e2dc52cc78282a
alexsamokhin <samohin.flute@gmail.com>
2022-11-07 11:47:16 +0200

    hw-08 v.1.0

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe')
+const player = new Player(iframe)
+
+player.on('timeupdate', throttle(onPlay, 1000))
+
+function onPlay(data){
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
+}
+
+let time = 0
+if (localStorage) {
+    try {
+        time = JSON.parse(localStorage.getItem("videoplayer-current-time"))
+    } catch (error) {}   
+}else{
+    time = 0
+}
+player.setCurrentTime(time)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle'
 
+const form = document.querySelector('.feedback-form')
+const email = document.querySelector('[type="email"]')
+const textarea = document.querySelector('[name="message"]')
+const submitBtn = document.querySelector('[type="submit"]')
+
+let localStorageData
+try {
+    localStorageData = JSON.parse(localStorage.getItem("feedback-form-state"))
+    if(!localStorageData){
+        email.value = ''
+        textarea.value = ''
+    } else {
+        email.value = localStorageData.email
+        textarea.value = localStorageData.textarea
+    }
+} catch (error) {
+    console.log('Error')
+}
+
+form.addEventListener('input', throttle(onFormInput, 500))
+submitBtn.addEventListener('click', onSubmit)
+
+let formObj = {...localStorageData}
+
+function onFormInput(){
+    formObj.email = email.value
+    formObj.textarea = textarea.value
+    localStorage.setItem("feedback-form-state", JSON.stringify(formObj))
+}
+
+function onSubmit(evt){
+    evt.preventDefault()
+    console.log(formObj)
+    formObj = {}
+    localStorage.clear()
+    form.reset()
+}
